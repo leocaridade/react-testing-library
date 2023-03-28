b0ab1afe28809c37cf360f00f267cbf194e50a2c
leocaridade <leocaridade@hotmail.com>
2023-03-28 15:57:19 -0400

    Requisito 4 concluido

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+const NOT_FOUND_SRC = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+describe('Testa o componente "Not Found"', () => {
+  it('A página contém um heading "h2" com o texto "Page requested not found"', () => {
+    const { history } = renderWithRouter(<App />);
+    act(() => {
+      history.push('digimon');
+    });
+    const notFoundTitle = screen.getByRole('heading', {
+      name: /page requested not found/i,
+      level: 2,
+    });
+    const notFoundImg = screen.getByRole('img', {
+      name: /pikachu crying because the page requested was not found/i,
+      src: NOT_FOUND_SRC,
+    });
+
+    expect(notFoundTitle).toBeInTheDocument();
+    expect(notFoundImg).toBeInTheDocument();
+    expect(notFoundImg.src).toBe(NOT_FOUND_SRC);
+  });
+});
